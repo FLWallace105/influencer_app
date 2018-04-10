dfec3e8180b8a0ca2b998aac3a69d9aae0266d23
Durrell Chamorro <durrell.chamorro@gmail.com>
2018-04-10 11:27:00 -0700

    write how to use this app section.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ require 'resque/server'
 
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
     root to: 'visitors#home'
   end
