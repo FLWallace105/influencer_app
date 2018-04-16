dda3bd8ca740c612d204c2f6ae4f6165d7cee3d4
Durrell Chamorro <durrell.chamorro@gmail.com>
2018-04-16 13:27:11 -0700

    revert to working resque config

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,9 +1,3 @@
+Resque.redis = 'localhost:6379'
 Resque.logger.level = Logger::DEBUG
 Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
-
-rails_root = Rails.root
-rails_env = Rails.env
-config_file = rails_root + 'config/resque.yml'
-
-resque_config = YAML::load(ERB.new(IO.read(config_file)).result)
-Resque.redis = resque_config[rails_env]
